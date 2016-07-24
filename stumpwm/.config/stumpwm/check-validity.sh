e69233e06b91faa793c528a98970e1ed86a0b928
Graham Dobbins <gdobbins@protonmail.com>
2016-07-24 14:53:27 -0400

    Improve stumpwm check-validity script

--- stumpwm/.config/stumpwm/check-validity.sh
+++ stumpwm/.config/stumpwm/check-validity.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 function checker(){
-    2>/dev/null git --git-dir=$HOME/$1/.git verify-commit HEAD
+    2>/dev/null git --git-dir="$HOME/$1/.git" verify-commit HEAD
 }
 
-if checker dotfiles && checker .emacs.d; then
-    echo -n "Live long and prosper."
+if checker dotfiles && checker .emacs.d \
+   && [ ! -f ~/.stumpwmrc ] \
+   && [ ! -d ~/.stumpwm.d ]; then
+    echo -n "I am always doing things I can't do. That is how I get to do them."
 else
     echo -n "*Warning: dotfiles may be compromised!!*"
 fi
